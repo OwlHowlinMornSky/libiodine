1a4aac1c2b51dabf81833575cc4031168eb3a0f0
Tyler Parret True <mysteryworldgod@outlook.com>
2024-12-30 19:12:32 +0800

    update c interface

--- include/libiodine.h
+++ include/libiodine.h
@@ -1,5 +1,5 @@
-#ifndef LIB_CAESIUM_H
-#define LIB_CAESIUM_H
+#ifndef LIB_CAESIUM_IODIDE_H
+#define LIB_CAESIUM_IODIDE_H
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,22 +9,22 @@ extern "C" {
 #include <stdbool.h>
 #include <stdlib.h>
 
-typedef enum SupportedFileTypes {
+typedef enum CSI_SupportedFileTypes {
     Jpeg = 0,
     Png = 1,
     Gif = 2,
     WebP = 3,
     Tiff = 4,
     Unkn = 5
-} SupportedFileTypes;
+} CSI_SupportedFileTypes;
 
-typedef struct CCSResult {
+typedef struct CSI_Result {
     bool success;
     uint32_t code;
     char *error_message;
-} CCSResult;
+} CSI_Result;
 
-typedef struct CCSParameters {
+typedef struct CSI_Parameters {
     bool keep_metadata;
     uint32_t jpeg_quality;
     uint32_t jpeg_chroma_subsampling; // support 444, 422, 420, 411
@@ -39,13 +39,19 @@ typedef struct CCSParameters {
     bool  optimize;
     uint32_t  width;
     uint32_t  height;
-} CCSParameters;
+} CSI_Parameters;
 
-CCSResult c_compress(const char* input_path, const char* output_path, CCSParameters* params);
+CSI_Result csi_compress(const char* input_path, const char* output_path, CSI_Parameters* params);
 
-CCSResult c_compress_to_size(const char* input_path, const char* output_path, CCSParameters* params, uint64_t max_output_size, bool return_smallest);
+CSI_Result csi_compress_into(const char* input_path, void* output_buffer, uint64_t obufmaxlen, CSI_Parameters* params);
 
-CCSResult c_convert(const char* input_path, const char* output_path, SupportedFileTypes format, CCSParameters* params);
+CSI_Result csi_compress_to_size(const char* input_path, const char* output_path, CSI_Parameters* params, uint64_t max_output_size, bool return_smallest);
+
+CSI_Result csi_compress_to_size_into(const char* input_path, void* output_buffer, uint64_t obufmaxlen, CSI_Parameters* params, uint64_t max_output_size, bool return_smallest);
+
+CSI_Result csi_convert(const char* input_path, const char* output_path, CSI_SupportedFileTypes format, CSI_Parameters* params);
+
+CSI_Result csi_convert_into(const char* input_path, void* output_buffer, uint64_t obufmaxlen, CSI_SupportedFileTypes format, CSI_Parameters* params);
 
 #ifdef __cplusplus
 }
