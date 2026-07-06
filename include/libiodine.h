af0cd7c4e332668cd29579c31c567f97766fda4a
Tyler True <TylerTrueZ@outlook.com>
2026-07-06 13:25:19 +0800

    Fewer differences to upstream

--- include/libiodine.h
+++ include/libiodine.h
@@ -5,77 +5,140 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
+#include <stdarg.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
 
-typedef enum CSI_SupportedFileTypes {
-    Jpeg = 0,
-    Png = 1,
-    Gif = 2,
-    WebP = 3,
-    Tiff = 4,
-    Unkn = 5
-} CSI_SupportedFileTypes;
+typedef enum SupportedFileTypes {
+  Jpeg,
+  Png,
+  Gif,
+  WebP,
+  Tiff,
+  Unkn,
+} SupportedFileTypes;
 
-typedef struct CSI_Result {
-    bool success;
-    uint64_t code;
-    const char *error_message;
-} CSI_Result;
+typedef struct CCSResult {
+  bool success;
+  uint32_t code;
+  const char *error_message;
+} CCSResult;
 
-typedef struct CSI_Parameters {
-    bool keep_metadata = false;
-    uint32_t jpeg_quality = 80;
-    uint32_t jpeg_chroma_subsampling = 0; // support 444, 422, 420, 411
-    bool jpeg_progressive = true;
-    bool jpeg_optimize = false;
-    bool jpeg_preserve_icc = true;
-    uint32_t  png_quality = 80;
-    uint32_t  png_optimization_level = 3;
-    bool  png_force_zopfli = false;
-    bool  png_optimize = false;
-    uint32_t  gif_quality = 80;
-    uint32_t  webp_quality = 80;
-    bool webp_lossless = false;
-    uint32_t  tiff_compression = 2; // support 1:Lzw 2:Deflate 3:Packbits Other Int:Uncompressed
-    uint32_t  tiff_deflate_level = 6; // support 1:Fast 6:Balanced Other Int:Best
-    uint32_t  width = 0;
-    uint32_t  height = 0;
-    bool  allow_magnify = false;
-    bool  reduce_by_power_of_2 = false;
-    uint32_t short_side_pixels = 0;
-    uint32_t long_size_pixels = 0;
-} CSI_Parameters;
+typedef struct CCSParameters {
+  bool keep_metadata = false;
+  uint32_t jpeg_quality = 80;
+  uint32_t jpeg_chroma_subsampling = 0; // support 444, 422, 420, 411
+  bool jpeg_progressive = true;
+  bool jpeg_optimize = false;
+  bool jpeg_preserve_icc = true;
+  uint32_t  png_quality = 80;
+  uint32_t  png_optimization_level = 3;
+  bool  png_force_zopfli = false;
+  bool  png_optimize = false;
+  uint32_t  gif_quality = 80;
+  uint32_t  webp_quality = 80;
+  bool webp_lossless = false;
+  uint32_t  tiff_compression = 2; // support 1:Lzw 2:Deflate 3:Packbits Other Int:Uncompressed
+  uint32_t  tiff_deflate_level = 6; // support 1:Fast 6:Balanced Other Int:Best
+  uint32_t  width = 0;
+  uint32_t  height = 0;
+  bool  allow_magnify = false;
+  bool  reduce_by_power_of_2 = false;
+  uint32_t short_side_pixels = 0;
+  uint32_t long_size_pixels = 0;
+} CCSParameters;
 
 typedef struct CByteArray {
   uint8_t *data;
   uintptr_t length;
-} CSI_ByteArray;
+} CByteArray;
 
-CSI_Result csi_compress(const char* input_path, const char* output_path, CSI_Parameters* params);
+struct CCSResult c_compress(const char *input_path,
+                            const char *output_path,
+                            struct CCSParameters params);
 
-CSI_Result csi_compress_into(const char* input_path, void* output_buffer, uint64_t obufmaxlen, CSI_Parameters* params);
+struct CCSResult c_compress_in_memory(const uint8_t *input_data,
+                                      uintptr_t input_length,
+                                      struct CCSParameters params,
+                                      struct CByteArray *output);
 
-CSI_Result csi_compress_fromto(const void* input_buffer, uint64_t ibuflen, void* output_buffer, uint64_t obufmaxlen, CSI_Parameters* params);
+struct CCSResult c_compress_to_size(const char *input_path,
+                                    const char *output_path,
+                                    struct CCSParameters params,
+                                    uintptr_t max_output_size,
+                                    bool return_smallest);
 
-CSI_Result csi_compress_in_memory(const uint8_t *input_data, uintptr_t input_length, struct CSI_Parameters params, struct CSI_ByteArray *output);
+struct CCSResult c_convert(const char *input_path,
+                           const char *output_path,
+                           enum SupportedFileTypes format,
+                           struct CCSParameters params);
 
-CSI_Result csi_compress_to_size(const char* input_path, const char* output_path, CSI_Parameters* params, uint64_t max_output_size, bool return_smallest);
+void c_free_byte_array(struct CByteArray byte_array);
 
-CSI_Result csi_compress_to_size_into(const char* input_path, void* output_buffer, uint64_t obufmaxlen, CSI_Parameters* params, uint64_t max_output_size, bool return_smallest);
+void c_free_string(char *ptr);
 
-CSI_Result csi_compress_to_size_fromto(const void* input_buffer, uint64_t ibuflen, void* output_buffer, uint64_t obufmaxlen, CSI_Parameters* params, uint64_t max_output_size, bool return_smallest);
 
-CSI_Result csi_convert(const char* input_path, const char* output_path, CSI_SupportedFileTypes format, CSI_Parameters* params);
 
-CSI_Result csi_convert_into(const char* input_path, void* output_buffer, uint64_t obufmaxlen, CSI_SupportedFileTypes format, CSI_Parameters* params);
+void iod_free_buffer(CByteArray byte_array);
 
-CSI_Result csi_convert_fromto(const void* input_buffer, uint64_t ibuflen, void* output_buffer, uint64_t obufmaxlen, CSI_SupportedFileTypes format, CSI_Parameters* params);
+struct CCSResult iod_compress_in_memory(
+    const uint8_t *input_data,
+    uintptr_t input_length,
+    CCSParameters params,
+    CByteArray *output
+);
 
-void csi_free_byte_array(struct CSI_ByteArray byte_array);
+struct CCSResult csi_compress_into(
+    const char* input_path,
+    void* output_buffer,
+    uint64_t obufmaxlen,
+    CCSParameters* params
+);
 
-void csi_free_string(char *ptr);
+struct CCSResult csi_compress_fromto(
+    const void* input_buffer,
+    uint64_t input_length,
+    void* output_buffer,
+    uint64_t obufmaxlen,
+    CCSParameters* params
+);
+
+struct CCSResult csi_compress_to_size_into(
+    const char* input_path,
+    void* output_buffer,
+    uint64_t obufmaxlen,
+    CCSParameters* params,
+    uint64_t max_output_size,
+    bool return_smallest
+);
+
+struct CCSResult csi_compress_to_size_fromto(
+    const void* input_buffer,
+    uint64_t input_length,
+    void* output_buffer,
+    uint64_t obufmaxlen,
+    CCSParameters* params,
+    uint64_t max_output_size,
+    bool return_smallest
+);
+
+struct CCSResult csi_convert_into(
+    const char* input_path,
+    void* output_buffer,
+    uint64_t obufmaxlen,
+    SupportedFileTypes format,
+    CCSParameters* params
+);
+
+struct CCSResult csi_convert_fromto(
+    const void* input_buffer,
+    uint64_t input_length,
+    void* output_buffer,
+    uint64_t obufmaxlen,
+    SupportedFileTypes format,
+    CCSParameters* params
+);
 
 #ifdef __cplusplus
 }
